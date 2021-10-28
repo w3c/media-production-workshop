7226521914c776c358856fc56520a307a0de62cb
Francois Daoust <fd@tidoust.net>
2021-10-28 17:21:07 +0200

    Add anchors to paragraphs with IDs to ease referencing

--- script.js
+++ script.js
@@ -21,7 +21,10 @@
     };
 
     window.addEventListener("load", function () {
-        Array.prototype.forEach.call(document.querySelectorAll("#main h1[id], #main h2[id], #main h3[id], #main h4[id], #main h5[id], #main section[id]>h2, #main section[id]>h3, #main section[id]>h4"), function (el) {
+        Array.prototype.forEach.call(document.querySelectorAll("#main h1[id], #main h2[id], #main h3[id], #main h4[id], #main h5[id], #main section[id]>h2, #main section[id]>h3, #main section[id]>h4, #main p[id]"), function (el) {
+            if (el.id === "buttonbar") {
+                return;
+            }
             var a = createAnchorFromHeading(el);
             el.classList.add("has-ref");
             el.addEventListener("click", function () {
